31f14085f35b3f84a74b461950d3008a905eb74d
aaron <aaron.m.bray@gmail.com>
2015-11-20 21:36:48 -0600

    Removed very early garbage test code (used to test the calling convention fixup glue code)

--- EntomoidUI/main.cpp
+++ EntomoidUI/main.cpp
@@ -7,48 +7,6 @@
 #include "WinUtils.hpp"
 
 using namespace entomoid;
-class BaseTest {
-public:
-	virtual void test() = 0;
-	virtual ~BaseTest() {}
-};
-HWND hwin;
-LRESULT CALLBACK WndFunc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
-{
-
-	return DefWindowProc(hwnd, msg, wparam, lparam);
-}
-
-class TestClass : public virtual BaseTest {
-private:
-	std::shared_ptr<void> cb_;
-	std::string testString;
-public:
-	virtual void test() override
-	{
-
-	}
-
-	virtual DWORD cb(PVOID f, int a, int b, int c)
-	{
-		std::cout << "Derp " << " " << (int)f << " " << a << " " << b << " " << c << " " << testString << std::endl;
-		return 10;
-	}
-
-	void init()
-	{
-		auto fpt = &TestClass::cb;
-		testString = "asdfasdf";
-		std::cout << "Init: " << std::hex << " 0x" << (size_t&)fpt << std::endl;
-		cb_ = entomoid::utils::makeClosure(this, fpt);
-	}
-
-
-	auto getCb()
-	{
-		return cb_;
-	}
-};
 
 int WINAPI WinMain(HINSTANCE hinst, HINSTANCE hPrev, LPSTR cmd, int cmdShow)
 {
