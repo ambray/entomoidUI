c89cf9605844669321691bc8f4e78e1005996f87
aaron <aaron.m.bray@gmail.com>
2015-11-29 22:19:34 -0600

    Fixed build script reference path

--- EntomoidUI/gen_builder.bat
+++ EntomoidUI/gen_builder.bat
@@ -7,7 +7,7 @@ if "%1"=="x64" (
    call cmake.exe -G "Visual Studio 14 2015 Win64" CMakeLists.txt
 )
 if "%1"=="android" (
-    call cmake.exe -DCMAKE_TOOLCHAIN_FILE="..\android.toolchain.cmake" -DANDROID_NDK=%HOMEPATH%\android-ndk-r10e -DANDROID_ABI="x86" -DCMAKE_BUILD_TYPE=Debug -GNinja
+    call cmake.exe -DCMAKE_TOOLCHAIN_FILE=".\build\android.toolchain.cmake" -DANDROID_NDK=%HOMEPATH%\android-ndk-r10e -DANDROID_ABI="x86" -DCMAKE_BUILD_TYPE=Debug -GNinja
     call cmake.exe --build .
     goto END
 )
